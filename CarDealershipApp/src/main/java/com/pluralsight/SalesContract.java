34635ca322fcadd0c1547a4f71ea0d23889b084e
Guy Arevalo <arevaloguy@gmail.com>
2024-05-19 17:36:15 -0700

    sales contract getters and setters + constructor

---
+++ CarDealershipApp/src/main/java/com/pluralsight/SalesContract.java
@@ -0,0 +1,59 @@
+package com.pluralsight;
+
+public class SalesContract extends Contract {
+    private double salesTax = .05;
+    private double recordingFee = 100;
+    private double processingFee;
+    private boolean finance;
+
+    public SalesContract(String date, String customerName, String customerEmail, String vehicleSold, double salesTax, double recordingFee, double processingFee, boolean finance) {
+        super(date, customerName, customerEmail, vehicleSold);
+        this.salesTax = salesTax;
+        this.recordingFee = recordingFee;
+        this.processingFee = processingFee;
+        this.finance = finance;
+    }
+
+    public double getSalesTax() {
+        return salesTax;
+    }
+
+    public void setSalesTax(double salesTax) {
+        this.salesTax = salesTax;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(double recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinance() {
+        return finance;
+    }
+
+    public void setFinance(boolean finance) {
+        this.finance = finance;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        return 0;
+    }
+    
+}
